9cf513aff0fb16a3a8ccf8f6f3c38dee3e41f6c5
lukasjast <lukasz.jast@icloud.com>
2024-03-10 22:13:52 +0100

    working

--- Secret_Auction.py
+++ Secret_Auction.py
@@ -7,6 +7,21 @@ def new_bidder(bidder_name, bid_amount, bidder_age):
     }
     bidders_list[bidder_name] = bidder_info
 
-new_bidder("John", 100, 19)
+def find_winner(bidders):
+    highest_bid = 0
+    winner = None
+    for bidder, info in bidders.items():
+        if info["bid"] > highest_bid:
+            highest_bid = info["bid"]
+            winner = bidder
+    print(f"The winner is {winner}.")
 
-print(bidders_list)
+players_amount = int(input("Welcome to the secret auction program. \nHow many players are you expecting ? "))
+
+for i in range(players_amount):
+    name = input(f"What's the name of the player {i+1}? ")
+    age = int(input(f"How old is the player {i+1}? "))
+    bid = int(input(f"What's his bid "))
+    new_bidder(name, bid, age)
+
+find_winner(bidders_list)
