bf9b3424fb43637b0afcef664316bcb44ef8e2e9
lukasjast <lukasz.jast@icloud.com>
2024-03-10 21:38:18 +0100

    new bidder

--- Secret_Auction.py
+++ Secret_Auction.py
@@ -1,1 +1,12 @@
-print("Hello world")
+bidders_list = {}
+
+def new_bidder(bidder_name, bid_amount, bidder_age):
+    bidder_info = {
+        "bid": bid_amount,
+        "age": bidder_age,
+    }
+    bidders_list[bidder_name] = bidder_info
+
+new_bidder("John", 100, 19)
+
+print(bidders_list)
