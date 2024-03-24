55f7b9c5a05417c393c04ee4df4e5fc69ac124f5
lukasjast <lukasz.jast@icloud.com>
2024-03-24 13:30:14 +0100

    do converting function

---
+++ day10.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    converted_f_name = f_name.title()
+    converted_l_name = l_name.title()
+
+    print(f"{converted_f_name} {converted_f_name}")
+
+format_name("JoHn", "jOhn")
